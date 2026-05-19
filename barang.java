7e4120df7989d1dc5b23805d2b8e902613beb551
aryooww <setyoaryowiguno@gmail.com>
2026-05-19 20:22:09 +0700

    test

---
+++ barang.java
@@ -0,0 +1,17 @@
+public class Barang {
+    private String nama;
+    private double harga;
+
+    public Barang(String nama, double harga) {
+        this.nama = nama;
+        this.harga = harga;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public double getHarga() {
+        return harga;
+    }
+}
