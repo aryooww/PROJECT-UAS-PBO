de186e95b5e4ea67ada44097e8b8146eadfe0b6b
aryooww <setyoaryowiguno@gmail.com>
2026-05-23 22:47:59 +0700

    getter, setter + implement method dari classbarang

--- model/barangElektronik.java
+++ model/barangElektronik.java
@@ -1,6 +1,6 @@
 package model;
 
-// class absbract yg mewarisi class barang
+// class abstract yg mewarisi class barang
 public abstract class barangElektronik extends barang {
     protected String merk;
     protected String warna;
@@ -11,5 +11,29 @@ public abstract class barangElektronik extends barang {
         this.merk = merk;
         this.warna = warna;
     }
+}
+
+// getter
+public String getMerk() {
+    return merk;
+}
+
+public String getWarna() {
+    return warna;
+}
+
+// setter
+public void setMerk(String merk) {
+    this.merk = merk;
+}
+
+public void setWarna(String warna) {
+    this.warna = warna;
+}
+
+// implementasi method abstract dari class barang
+@Override
+public String getJenisBarang() {
+    return "Elektronik";
 }
 
